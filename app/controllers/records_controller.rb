2eab4eee81bfb2fecc8be815d9bc4bb14acc0b5c
ruffini47 <ruffini47@gmail.com>
2020-04-18 03:58:17 +0000

    プレイ情報の更新、削除ボタンを設置し機能させる。validation追加、エラーメッセージ追加。

--- app/controllers/records_controller.rb
+++ app/controllers/records_controller.rb
@@ -1,4 +1,5 @@
 class RecordsController < ApplicationController
+  before_action :set_user, only: [:edit, :update, :destroy]
   def new
     @record = Record.new          
   end
@@ -7,10 +8,10 @@ class RecordsController < ApplicationController
     @record = Record.new(records_params)
     @record.user_id = params[:user_id]
     if @record.save
-      flash[:success] = "プレイヤー情報を更新しました。"
+      flash[:success] = "プレイヤー情報を作成しました。"
       redirect_to show_board_url(@record.id)
     else
-      flash[:danger] = "プレイヤー情報の更新に失敗しました。"
+      flash[:danger] = "プレイヤー情報の作成に失敗しました。"
       render :new     
     end
   end 
@@ -19,6 +20,27 @@ class RecordsController < ApplicationController
     @records = Record.all
     @records = @records.where(user_id: current_user.id)
   end
+
+  def edit
+  end
+
+  def update
+    if @record.update_attributes(records_params)
+      flash[:success] = "プレイヤー情報を更新しました。"
+      redirect_to user_records_path(current_user.id)
+    else
+      flash[:danger] = "プレイヤー情報の更新に失敗しました。"
+      render :edit      
+    end
+  end
+
+  def destroy
+    @record.destroy
+    flash[:success] = "「#{@record.title}」のデータを削除しました。"
+    redirect_to user_records_path(current_user.id)
+  end
+
+
 
   private
 
