334a30093ddd3176f5b01da3186720ef87ebb50b
ruffini47 <ruffini47@gmail.com>
2020-08-17 11:37:22 +0000

    トップページで検索対象が無い場合、検索結果を表示する（棋譜）ボタンは無効にするで、current_userの検索対象が無い場合に制限する

--- app/helpers/static_pages_helper.rb
+++ app/helpers/static_pages_helper.rb
@@ -1,6 +1,6 @@
 module StaticPagesHelper
   def searched?
-    game_record = GameRecord.find_by(searched: true)
+    game_record = GameRecord.where(user_id: current_user.id).find_by(searched: true)
     game_record.present? 
   end
 end
